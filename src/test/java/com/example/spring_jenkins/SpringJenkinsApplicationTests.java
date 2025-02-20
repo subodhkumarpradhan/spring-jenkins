aad01b687aeedec94fc962524453d6ff0c4737ed
Kum@r123 <subodh.msu@gmail.com>
2025-02-21 03:13:15 +0530

    fixed issue

--- src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/spring_jenkins/SpringJenkinsApplicationTests.java
@@ -15,7 +15,7 @@ class SpringJenkinsApplicationTests {
 	static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	 contextLoads() {
+	void contextLoads() {
 		logger.info("Test case executing...");
 		logger.info("Test case executing second log statement...");
 		assertEquals(true, true);
